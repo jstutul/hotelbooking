965b516403d14dd159dafccc05c6b04e89c1b2c6
jstutul <jaforsadiktutul9228@gmail.com>
2022-08-27 23:48:19 +0600

    complete reservation

--- src/components/reserve/Reserve.js
+++ src/components/reserve/Reserve.js
@@ -1,14 +1,98 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
-const Reserve=({setOpen,hotelId})=> {
+import React, { useContext } from "react";
+import "./Reserve.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark } from "@fortawesome/free-regular-svg-icons";
+import useFetch from "../../hooks/useFetch";
+import { useState } from "react";
+import { SearchContext } from "../../context/SearchContext";
+import axios from "axios";
+const Reserve = ({ setOpen, hotelId }) => {
+  const [selectRooms, setSelectRooms] = useState([]);
+  const { dates } = useContext(SearchContext);
+  const { data } = useFetch(`http://localhost:5000/api/hotels/room/${hotelId}`);
+
+  const handleSelect = (e) => {
+    const checked = e.target.checked;
+    const value = e.target.value;
+    setSelectRooms(
+      checked
+        ? [...selectRooms, value]
+        : selectRooms.filter((item) => item !== value)
+    );
+  };
+  const getDateInRange = (startDate, endDate) => {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    const date = new Date(start.getTime());
+    let dates = [];
+    while (date <= end) {
+      dates.push(new Date(date).getTime());
+      date.setDate(date.getDate() + 1);
+    }
+    return dates;
+  };
+  const alldates = getDateInRange(dates[0].startDate, dates[0].endDate);
+
+  const isAvailable = (roomNumber) => {
+    const isFound = roomNumber.unavailableDates.some((date) =>
+      alldates.includes(new Date(date).getTime())
+    );
+
+    return !isFound;
+  };
+
+  const handleClick = async () => {
+    try {
+      await Promise.all(
+        selectRooms.map((roomId) => {
+          const res = axios.put(
+            `http://localhost:5000/api/rooms/availability/${roomId}`,
+            { dates: alldates }
+          );
+          return res.data;
+        })
+      );
+    } catch (err) {}
+  };
   return (
-    <div className='reserve'>
-        <div className='rContainer'>
-            <FontAwesomeIcon icon={faCircleXmark} className="rClose" onClick={()=>setOpen(false)} />
-            <span>Select your Rooms</span>
-        </div>
+    <div className="reserve">
+      <div className="rContainer">
+        <FontAwesomeIcon
+          icon={faCircleXmark}
+          className="rClose"
+          onClick={() => setOpen(false)}
+        />
+        <span>Select your Rooms</span>
+        {data.map((item) => (
+          <div className="rItem">
+            <div className="rItemInfo">
+              <div className="rTitle">{item.title}</div>
+              <div className="rDesc">{item.desc}</div>
+              <div className="rMax">
+                Max People : <b>{item.maxPeople}</b>
+              </div>
+              <div className="rPrice">{item.price}</div>
+            </div>
+            <div className="rSelectRooms">
+              {item.roomNumbers.map((roomNumber) => (
+                <div className="room" key={roomNumber._id}>
+                  <label>{roomNumber.number}</label>
+                  <input
+                    type="checkbox"
+                    value={roomNumber._id}
+                    onChange={handleSelect}
+                    disabled={!isAvailable(roomNumber)}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+        <button className="rButton" onClick={handleClick}>
+          Reserve Now
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 export default Reserve;
--- src/pages/hotel/Hotel.js
+++ src/pages/hotel/Hotel.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import "./Hotel.css";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
-import {AuthContext} from "../../context/AuthContext"
-import { useNavigate } from 'react-router-dom';
+import { AuthContext } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 import {
   faLocationDot,
   faCircleXmark,
@@ -19,26 +19,6 @@ import { useContext } from "react";
 import { SearchContext } from "../../context/SearchContext";
 import Reserve from "../../components/reserve/Reserve";
 
-const photos = [
-  {
-    src: "https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-  },
-  {
-    src: "https://media.istockphoto.com/photos/interior-of-a-modern-luxury-hotel-double-bed-bedroom-picture-id1163498940?k=20&m=1163498940&s=612x612&w=0&h=tUPidNW2ny095sCR97dur7cbrCnYpcjHbevUTJyB8Jc=",
-  },
-  {
-    src: "https://thumbs.dreamstime.com/z/hotel-room-27254386.jpg",
-  },
-  {
-    src: "https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aG90ZWwlMjByb29tfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
-  },
-  {
-    src: "https://st.depositphotos.com/1164721/2078/i/600/depositphotos_20784379-stock-photo-interior-of-modern-comfortable-hotel.jpg",
-  },
-  {
-    src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSYsq0keW82D9Nb6uSvic9jfekRq8c3d3Lv7LeW2K0k5tQ6oGMrF-nFNdGh_6Jt2HbEmo&usqp=CAU",
-  },
-];
 const Hotel = () => {
   const [sliderNumber, setSliderNumber] = useState(0);
   const [open, setOpen] = useState(false);
@@ -47,16 +27,16 @@ const Hotel = () => {
   const id = location.pathname.split("/")[2];
   const url = `http://localhost:5000/api/hotels/find/${id}`;
   const { data, loading } = useFetch(url);
-  const {dates,options}=useContext(SearchContext);
-  const MILLISECOND_PER_DAY=1000*60*60*24;
-  function dayDifference(date1,date2){
-    const timeDiff=Math.abs(date2.getTime()-date1.getTime());
-    const diffDays=Math.ceil(timeDiff/MILLISECOND_PER_DAY);
+  const { dates, options } = useContext(SearchContext);
+  const MILLISECOND_PER_DAY = 1000 * 60 * 60 * 24;
+  function dayDifference(date1, date2) {
+    const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    const diffDays = Math.ceil(timeDiff / MILLISECOND_PER_DAY);
     return diffDays;
   }
-  const days=dayDifference(dates[0].endDate,dates[0].startDate);
+  console.log(dates);
+  const days = dayDifference(dates[0].endDate, dates[0].startDate);
 
-  
   const handleOpen = (i) => {
     setSliderNumber(i);
     setOpen(true);
@@ -71,15 +51,15 @@ const Hotel = () => {
     }
     setSliderNumber(NewSliderNumber);
   };
-  const {user}=useContext(AuthContext)
-  const navigate=useNavigate();
-  const handleClick=()=>{
-    if(user){
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleClick = () => {
+    if (user) {
       setOpenModal(true);
-    }else{
-      navigate("/login")
+    } else {
+      navigate("/login");
     }
-  }
+  };
   return (
     <>
       <Navbar />
@@ -115,7 +95,9 @@ const Hotel = () => {
             </div>
           )}
           <div className="hotelWrapper">
-            <button onClick={handleClick} className="bookNow">Reserve or Book Now</button>
+            <button onClick={handleClick} className="bookNow">
+              Reserve or Book Now
+            </button>
             <h1 className="hotelTitle">{data.name}</h1>
             <div className="hotelAddress">
               <FontAwesomeIcon icon={faLocationDot} />
@@ -152,9 +134,10 @@ const Hotel = () => {
                   excelent location store of 9.8
                 </span>
                 <h2>
-                  <b>${days * data.cheapestPrice * options.room}</b>({days} nights)
+                  <b>${days * data.cheapestPrice * options.room}</b>({days}{" "}
+                  nights)
                 </h2>
-                <button>Reserve or Book Now</button>
+                <button onClick={handleClick}>Reserve or Book Now</button>
               </div>
             </div>
           </div>
@@ -162,7 +145,7 @@ const Hotel = () => {
           <Footer />
         </div>
       )}
-      {openModal && <Reserve setOpen={setOpenModal} hotelId={id} /> }
+      {openModal && <Reserve setOpen={setOpenModal} hotelId={id} />}
     </>
   );
 };
